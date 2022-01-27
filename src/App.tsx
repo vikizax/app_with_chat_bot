f4c942858607c7387e74870f085240b2b1384e43
Vivek Chetia <vivek.chetia@clinifyhealth.com>
2022-01-27 21:24:26 +0530

    for hosting

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useLayoutEffect, useEffect } from "react";
+import { isMobile } from "react-device-detect";
+import "./App.css";
+import ChatIcon from "./assets/chat.svg";
 
 function App() {
+  const [size, setSize] = useState([0, 0]);
+  const [mobile, setMobile] = useState(false);
+  const [visible, setVisible] = useState(false);
+
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight - 10]);
+      if (isMobile || window.innerWidth <= 768) setMobile(true);
+      else setMobile(false);
+    }
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        height: "100vh",
+      }}
+      onClick={(e) => {
+        e.stopPropagation();
+        if (visible) setVisible(false);
+      }}
+    >
+      <iframe
+        src="http://chat-bot-ui.vercel.app/"
+        frameBorder={0}
+        height={mobile ? size[1] : 626}
+        width={mobile ? size[0] : 400}
+        style={{
+          position: mobile ? "unset" : "fixed",
+          border: !mobile ? "1px solid #e4e4e4" : "unset",
+          borderRadius: !mobile ? "18px" : "unset",
+          visibility: visible ? "visible" : "hidden",
+          bottom: visible ? "2rem" : "-200rem",
+          transition: "all 0.5s ease-in-out",
+        }}
+      ></iframe>
+
+      <button
+        onClick={(e) => {
+          setVisible(true);
+          //@ts-ignore
+          window["chabot"] = true;
+        }}
+        style={{
+          opacity: !visible ? 1 : 0,
+          transition: "all 0.5s ease-in-out",
+          pointerEvents: visible ? "none" : "unset",
+        }}
+      >
+        <img src={ChatIcon} alt="chat bot icon" height={30} width={30} />
+      </button>
     </div>
   );
 }
