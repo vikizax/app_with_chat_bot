cbade84b43fb46e8aed878c310ee53356846494e
Vivek Chetia <vivek.chetia@clinifyhealth.com>
2022-01-27 22:01:15 +0530

    updated to support animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useLayoutEffect, useEffect } from "react";
+import { useState, useLayoutEffect, useEffect, useRef } from "react";
 import { isMobile } from "react-device-detect";
 import "./App.css";
 import ChatIcon from "./assets/chat.svg";
@@ -7,7 +7,7 @@ function App() {
   const [size, setSize] = useState([0, 0]);
   const [mobile, setMobile] = useState(false);
   const [visible, setVisible] = useState(false);
-
+  const iFrameRef = useRef<HTMLIFrameElement>(null);
   useLayoutEffect(() => {
     function updateSize() {
       setSize([window.innerWidth, window.innerHeight - 10]);
@@ -30,7 +30,7 @@ function App() {
       }}
     >
       <iframe
-        src="https://chat-bot-ui.vercel.app/"
+        ref={iFrameRef}
         frameBorder={0}
         height={mobile ? size[1] : 626}
         width={mobile ? size[0] : 400}
@@ -38,7 +38,7 @@ function App() {
           position: mobile ? "unset" : "fixed",
           border: !mobile ? "1px solid #e4e4e4" : "unset",
           borderRadius: !mobile ? "18px" : "unset",
-          visibility: visible ? "visible" : "hidden",
+          display: visible ? "visible" : "hidden",
           bottom: visible ? "2rem" : "-200rem",
           transition: "all 0.5s ease-in-out",
         }}
@@ -46,9 +46,10 @@ function App() {
 
       <button
         onClick={(e) => {
-          setVisible(true);
-          //@ts-ignore
-          window["chabot"] = true;
+          if (iFrameRef.current) {
+            iFrameRef.current.src = "https://chat-bot-ui.vercel.app/";
+            setVisible(true);
+          }
         }}
         style={{
           opacity: !visible ? 1 : 0,
